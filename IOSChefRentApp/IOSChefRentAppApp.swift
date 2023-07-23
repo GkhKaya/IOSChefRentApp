2cd1510f178c6f664fcd0a0a9d8a07609f4e9692
GkhKaya <gkhkaya00@gmail.com>
2023-07-23 11:02:18 +0300

    Initial Commit

---
+++ IOSChefRentApp/IOSChefRentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSChefRentAppApp.swift
+//  IOSChefRentApp
+//
+//  Created by Gokhan Kaya on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSChefRentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
